bf3fc9d823008f498069e913acd132d17f26a1d3
Sumit K <synergy2411@outlook.com>
2022-09-29 12:22:41 +0530

    graphql boilerplate added

---
+++ 00-graphql-boilerplate/src/graphql/resolvers/index.js
@@ -0,0 +1,8 @@
+import { Query } from './Query'
+import { Mutation } from './Mutation'
+import { Subscription } from './Subscription'
+
+
+const resolvers = { Query, Mutation, Subscription }
+
+export { resolvers }
