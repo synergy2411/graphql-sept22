a979fcaa152c491edfad544f911893672911a66c
Sumit K <synergy2411@outlook.com>
2022-09-29 12:57:19 +0530

    dot env added

--- 02-graphql-auth/src/db/index.js
+++ 02-graphql-auth/src/db/index.js
@@ -1,6 +1,10 @@
 import mongoose from 'mongoose';
+import * as dotenv from 'dotenv';
+dotenv.config()
 
-const mongoURI = "mongodb+srv://user123:vevVonnvHRiWkS87@cluster0.e9xsq.mongodb.net/optum-db?retryWrites=true&w=majority"
+const { mongoUser, mongoPassword } = process.env;
+
+const mongoURI = `mongodb+srv://${mongoUser}:${mongoPassword}@cluster0.e9xsq.mongodb.net/optum-db?retryWrites=true&w=majority`
 mongoose.connect(mongoURI)
     .then(() => {
         console.log("Mongo Connected")
