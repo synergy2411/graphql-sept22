09f4d60e2ce4e66eca9d3e3e489f71e385d3f209
Sumit K <synergy2411@outlook.com>
2022-09-29 13:57:19 +0530

    users found

--- 02-graphql-auth/src/graphql/resolvers/Query.js
+++ 02-graphql-auth/src/graphql/resolvers/Query.js
@@ -1,5 +1,21 @@
+import { GraphQLYogaError } from '@graphql-yoga/node';
+
 const Query = {
-    hello: () => "World"
+    hello: () => "World",
+    users: async (_, args, { UserModel }) => {
+        try {
+            const allUsers = await UserModel.find()
+            return allUsers.map(user => {
+                return {
+                    id: user._id,
+                    username: user.username,
+                    age: user.age
+                }
+            })
+        } catch (err) {
+            throw new GraphQLYogaError(err)
+        }
+    }
 }
 
 export { Query }
